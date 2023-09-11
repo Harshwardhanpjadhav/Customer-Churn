4d23ac89b794f141e20ff71354ca63e9a7c104f8
Harshwardhan <harshwardhanpj2001@gmail.com>
2023-09-11 22:26:24 +0530

    add environment variable

---
+++ src/churn/constants/environment_variable/__init__.py
@@ -0,0 +1,16 @@
+from dotenv import find_dotenv, load_dotenv
+from dataclasses import dataclass
+import os,sys
+
+# find_dotenv function finds the dot .env file from file 
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+@dataclass
+class EvironmentVariable:
+    """
+    This class is used to fetch environment variable from .env file
+    """
+
+    # MongoDB Url fetched from .env file (.env file is not pushed in github)
+    mongo_url = os.getenv('MONGODB_URL') 
