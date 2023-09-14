7c6c6c7d310f1ca046fe808fdf6742a049774aa2
Harshwardhan <harshwardhanpj2001@gmail.com>
2023-09-14 16:14:40 +0530

    #20  add training pipeline imports

--- src/churn/pipeline/training_pipeline.py
+++ src/churn/pipeline/training_pipeline.py
@@ -1,3 +1,12 @@
+from src.churn.pipeline.training_pipeline import TrainingPipeline as tp
+
+import os
+import sys
+from src.churn.logger import logging
+from src.churn.exception import CustomException
+from src.churn.entity.config import TrainingPipelineConfig
+
+
 class TrainingPipeline:
     '''
     This class is used to create the training pipeline object.
@@ -10,30 +19,30 @@ class TrainingPipeline:
         # Initialize the training pipeline configuration (This does not store the configuration)
         training_pipeline_config = TrainingPipelineConfig()
 
-    #Initiate data ingestion
-    #This will return a DataIngestionArtifact
-    def start_data_ingestion(self) -> DataIngestionArtifact: 
+    # Initiate data ingestion
+    # This will return a DataIngestionArtifact
+    def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
-            # Performing data ingestion 
+            # Performing data ingestion
             pass
 
         except Exception as e:
             # If an exception occurs during data ingestion, raise a custom exception with error 'e'
             raise CustomException(e, sys)
-    
-    #Initiate data validation
-    #This will return a DataValidationArtifact
+
+    # Initiate data validation
+    # This will return a DataValidationArtifact
     def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
         try:
-            # Performing data validation using the provided data ingestion artifact 
+            # Performing data validation using the provided data ingestion artifact
             pass
 
         except Exception as e:
             # If an exception occurs during data validation, raise a custom exception with error 'e'
             raise CustomException(e, sys)
 
-    #Initiate data transformation
-    #This will return a DataTransformationArtifact
+    # Initiate data transformation
+    # This will return a DataTransformationArtifact
     def start_data_transformation(self, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
         try:
             # Performing data transformation using the provided data validation artifact
@@ -43,22 +52,22 @@ class TrainingPipeline:
             # If an exception occurs during data transformation, raise a custom exception with error 'e'
             raise CustomException(e, sys)
 
-    #Initiating model trainer module
-    #This will return a ModelTrainerArtifact
+    # Initiating model trainer module
+    # This will return a ModelTrainerArtifact
     def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
         try:
-            #Training a machine learning model using the provided data transformation artifact 
+            # Training a machine learning model using the provided data transformation artifact
             pass
 
         except Exception as e:
             # If an exception occurs during model training, raise a custom exception with error 'e'
             raise CustomException(e, sys)
 
-    #Initiating model evaluation module
-    #This will return a ModelEvaluationArtifact
+    # Initiating model evaluation module
+    # This will return a ModelEvaluationArtifact
     def start_model_evaluation(self, model_trainer_artifact: ModelTrainerArtifact, data_validation_artifact: DataValidationArtifact) -> ModelEvaluationArtifact:
         try:
-            # Evaluating the trained model using the provided artifacts 
+            # Evaluating the trained model using the provided artifacts
             pass
 
         except Exception as e:
@@ -66,7 +75,7 @@ class TrainingPipeline:
             raise CustomException(e, sys)
 
     # Initiating model pusher module
-    #This will return a ModelPusherArtifact
+    # This will return a ModelPusherArtifact
 
     def start_model_pusher(self, model_eval_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
         try:
@@ -77,9 +86,8 @@ class TrainingPipeline:
             # If an exception occurs during model pushing, raise a custom exception with error 'e'
             raise CustomException(e, sys)
 
+    # This will execute the entire pipeline by arranging the different stages
 
-
-    #This will execute the entire pipeline by arranging the different stages
     def run_pipeline(self):
         try:
             pass
