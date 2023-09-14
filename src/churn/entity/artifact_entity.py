5ed7ae9e21f9b04d1b3f9ab08d833467c9c608ee
Naman <namanupadhye3@gmail.com>
2023-09-14 19:24:27 +0530

    Writing artifacts,issue #30

--- src/churn/entity/artifact_entity.py
+++ src/churn/entity/artifact_entity.py
@@ -10,8 +10,8 @@ class DataIngestionArtifact:
     trained_file_path: str  # Path to the training data file
     test_file_path: str     # Path to the test data file
 
-@dataclass
 # Data Validation Artifact includes information about data validation and potential issues
+@dataclass
 class DataValidationArtifact:
     validation_status: bool           # Indicates whether data validation was successful or not
     valid_train_file_path: str        # Path to the valid training data file
@@ -20,29 +20,29 @@ class DataValidationArtifact:
     invalid_test_file_path: str       # Path to the invalid test data file (if any issues found)
     drift_report_file_path: str       # Path to a report on data drift
 
-@dataclass
 # Data Transformation Artifact includes information related to data transformation
+@dataclass
 class DataTransformationArtifact:
     transformed_object_file_path: str  # Path to the transformed data object
     transformed_train_file_path: str   # Path to the transformed training data file
     transformed_test_file_path: str    # Path to the transformed test data file
 
-@dataclass
 # Classification Metric Artifact includes classification metrics
+@dataclass
 class ClassificationMetricArtifact:
     f1_score: float         # F1-score for the classification model
     precision_score: float  # Precision score for the classification model
     recall_score: float     # Recall score for the classification model
 
-@dataclass
 # Model Trainer Artifact includes information related to model training module
+@dataclass
 class ModelTrainerArtifact:
     trained_model_file_path: str       # Path to the trained machine learning model file
     train_metric_artifact: ClassificationMetricArtifact  # Metrics on the training data
     test_metric_artifact: ClassificationMetricArtifact   # Metrics on the test data
 
-@dataclass
 # Model Evaluation Artifact includes information related to model evaluation
+@dataclass
 class ModelEvaluationArtifact:
     is_model_accepted: bool        # Indicates whether the model is accepted
     improved_accuracy: float       # Improvement in model accuracy
@@ -51,8 +51,8 @@ class ModelEvaluationArtifact:
     train_model_metric_artifact: ClassificationMetricArtifact  
     best_model_metric_artifact: ClassificationMetricArtifact   
 
-@dataclass
 # Model Pusher Artifact includes information related to pushing/deploying the model
+@dataclass
 class ModelPusherArtifact:
     saved_model_path: str     # Path to the saved model fiel
     model_file_path: str      # Path to the model file
